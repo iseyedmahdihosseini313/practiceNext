125bb23282256d656c122c1870e601fb2aa9ee40
seyed <iseyedmahdihosseini313@gamil.com>
2024-11-18 03:49:12 +0330

    update submition

--- src/components/futures/common/form/form.tsx
+++ src/components/futures/common/form/form.tsx
@@ -1,17 +1,22 @@
+import { toast, Toaster } from 'sonner';
 import IForm from './form.type';
 const Form = ({children,handleSubmit}:IForm) => {
      return (
-          <form
-               autoComplete="off"
-               autoSave="off"
-               spellCheck="false"
-               onSubmit={handleSubmit((data: any) => {
-                    console.log(data);
-               })}
-               className="min-w-[80%] sm:min-w-[50%]  md:min-w-[27%]  p-4 gap-2 bg-blue-100/5 rounded-md ring-1 ring-cyan-200/5"
-          >
-               {children}
-          </form>
+          <>
+               <Toaster richColors position="top-right" expand={true} />
+               <form
+                    autoComplete="off"
+                    autoSave="off"
+                    spellCheck="false"
+                    onSubmit={handleSubmit((data: any) => {
+                         console.log(data);
+                         toast.success('Successfully action');
+                    })}
+                    className="min-w-[80%] sm:min-w-[50%]  md:min-w-[27%]  p-4 gap-2 bg-blue-100/5 rounded-md ring-1 ring-cyan-200/5"
+               >
+                    {children}
+               </form>
+          </>
      );
 };
 
